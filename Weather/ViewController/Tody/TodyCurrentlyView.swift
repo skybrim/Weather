e6f1fb7b74c6d02848888f5063995faabfd45ae9
skybrim <throughskybrim@gmail.com>
2019-12-30 23:47:42 +0800

    .#

---
+++ Weather/ViewController/Tody/TodyCurrentlyView.swift
@@ -0,0 +1,21 @@
+//
+//  TodyCurrentlyView.swift
+//  Weather
+//
+//  Created by wiley on 2019/12/30.
+//  Copyright © 2019 wiley. All rights reserved.
+//
+
+import UIKit
+
+class TodyCurrentlyView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
